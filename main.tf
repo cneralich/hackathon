cbccb823cab901c50b49cbb673aee0b65717dd5d
cneralich <corrigan@hashicorp.com>
2019-03-19 12:55:36 -0700

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,40 @@
+provider "kubernetes" {
+    host = "${var.host}"
+    token = "${var.token}"
+    insecure = true
+}
+
+resource "kubernetes_pod" "echo" {
+  metadata {
+    name = "echo-example"
+    labels {
+      App = "echo"
+    }
+  }
+  spec {
+    container {
+      image = "hashicorp/http-echo:0.2.1"
+      name  = "example2"
+      args = ["-listen=:80", "-text='Hello World'"]
+      port {
+        container_port = 80
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "echo" {
+  metadata {
+    name = "echo-example"
+  }
+  spec {
+    selector {
+      App = "${kubernetes_pod.echo.metadata.0.labels.App}"
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+    type = "LoadBalancer"
+ }
+}
---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "host_address" {}
+
+variable "token" {}
